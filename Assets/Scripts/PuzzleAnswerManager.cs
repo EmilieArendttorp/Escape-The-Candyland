bc573ff6b7c664a154721d6dc17f31a6c5cd9cb8
EmilieArendttorp <emiliearendttorp@gmail.com>
2021-10-16 21:14:17 +0200

    Button and random spawn script
    Help from André. Scripts are working.
    
    Continue from here.

---
+++ Assets/Scripts/PuzzleAnswerManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleAnswerManager : MonoBehaviour
+{
+    GameObject fairyObject = null, gingerObject = null, iceObject = null;
+
+    public void SetFairyObject(GameObject answer)
+    {
+        fairyObject = answer;
+    }
+
+    public void SetGingerObject(GameObject answer)
+    {
+        gingerObject = answer;
+    }
+
+    public void SetIceObject(GameObject answer)
+    {
+        iceObject = answer;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (CheckIfCorrectFairyAnswer(collision.gameObject))
+        {
+            //Write code here that should occur when answer is CORRECT.
+            return;
+        }
+
+        if (CheckIfCorrectGingerAnswer(collision.gameObject))
+        {
+            //Write code here that should occur when answer is CORRECT.
+            return;
+        }
+
+        if (CheckIfCorrectIceAnswer(collision.gameObject))
+        {
+            //Write code here that should occur when answer is CORRECT.
+            return;
+        }
+
+        //Write code here that should occur when answer is WRONG.
+    }
+
+    bool CheckIfCorrectFairyAnswer(GameObject objectToTest)
+    {
+        return objectToTest == fairyObject ? true : false;
+    }
+
+    bool CheckIfCorrectGingerAnswer(GameObject objectToTest)
+    {
+        return objectToTest == gingerObject ? true : false;
+    }
+
+    bool CheckIfCorrectIceAnswer(GameObject objectToTest)
+    {
+        return objectToTest == iceObject ? true : false;
+    }
+}
---
+++ Assets/Scripts/PuzzleButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleButton : MonoBehaviour
+{
+    public bool isAnswer = false;
+
+    Material material = null;
+
+    private void Awake()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    public void SetColor(Color color)
+    {
+        material.color = color;
+    }
+
+    public Color GetColor()
+    {
+        return material.color;
+    }
+}
